608decd0f051df4629fbbc820aebe2f317b26296
Prashant Rajput <prashantkumarrajput582008@gmail.com>
2025-10-19 17:37:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-let main_post = document.querySelector(".main_post")
-let sec_post = document.querySelector(".sec_post")
-let sub_but = document.querySelector(".sub_but")
-let follow_butt = document.querySelector(".follow_butt")
+let main_post = document.querySelector(".main_post");
+let sec_post = document.querySelector(".sec_post");
+let sub_but = document.querySelector(".sub_but");
+let follow_butt = document.querySelector(".follow_butt");
 
-main_post.addEventListener("click" , ()=>{
-    window.location.href = "other_pages/login.html" , "_blank";
-})
+main_post.addEventListener("click", () => {
+  window.open("other_pages/login.html", "_blank");
+});
 
-sec_post.addEventListener("click" , ()=>{
-    window.location.href = "other_pages/login.html" , "_blank";
-})
+sec_post.addEventListener("click", () => {
+  window.open("other_pages/login.html", "_blank");
+});
 
-sub_but.addEventListener("click" , ()=>{
-    window.location.href = "subscribe.html" , "_blank";
-})
+sub_but.addEventListener("click", () => {
+  window.open("subscribe.html", "_blank");
+});
 
-follow_butt.addEventListener("click" , ()=>{
-    window.location.href = "other_pages/login.html" , "_blank";
-})
+follow_butt.addEventListener("click", () => {
+  window.open("other_pages/login.html", "_blank");
+});
